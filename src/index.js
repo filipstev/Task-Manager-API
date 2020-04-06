a73ef96dfcad2342374b7c803c37b340b9ec5e5d
Filip <filipstevanovic176@gmail.com>
2020-04-06 22:13:59 +0200

    =added testing

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
-const express = require('express')
-require('./db/mongoose')
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-
-const app = express()
+const app = require('./app')
 const port = process.env.PORT
 
 //MULTER SETUP EXAMPLE
@@ -31,12 +26,6 @@ const port = process.env.PORT
 // }, (error, req, res, next) => {
 //     res.status(400).send({ error: error.message})
 // })
-
-
-
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
